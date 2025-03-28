cda78ffe7d48578d73a4ee9dea1d552a6ff9b79d
EnricoCiccolini <piroroma90@gmail.com>
2025-03-28 13:43:36 +0100

    feat: active server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3003
+
+app.use(express.static('public'))
+
+
+
+app.listen(port,()=>{
+    console.log(`sono sulla porta${port}`)
+})
